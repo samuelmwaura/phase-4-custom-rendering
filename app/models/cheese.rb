f14c8ff001700dc73f5442fbb8d2b89ef991b647
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-09-28 22:38:08 +0300

    commit tree 361cc385e70663d8f65cee98cd228804e0f53918

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -1,7 +1,9 @@
 class Cheese < ApplicationRecord
   
   def summary
-    "#{name}: $#{price}"
+    "#{name}: $#{price}" #The name and price will be returned without the self keyword since they are defined as instance methods
+    #This method can be included in the return Json inorder to extend the Json data that is returned.
+    #Since it is an instance method it will be called for every instance that is being returned.
   end
 
 end
